31561e458435476fbbaf6b33ef30d2084dfd0b04
Wes Oldenbeuving <narnach@gmail.com>
2009-12-03 15:42:25 +0100

    Use bit.ly URL shortening and latest version of Twitter gem

--- github-twitter.rb
+++ github-twitter.rb
@@ -3,33 +3,42 @@ require 'json'
 require 'twitter'
 require 'sinatra'
 require 'erb'
+require 'bitly'
 
 REPOS = YAML.load_file('config.yml')
 
 class GithubTwitter
-  
+
   def initialize(payload)
     payload = JSON.parse(payload)
     return unless payload.keys.include?("repository")
     @repo = payload["repository"]["name"]
-    @template = ERB.new(REPOS[@repo]["template"] || "[<%= commit['repo'] %>] <%= commit['url'] %> by <%= commit['author']['name'] %> - <%= commit['message'] %>")
+    @template = ERB.new(REPOS[@repo]["template"])
     @twitter = connect(@repo)
-    payload["commits"].each { |c| process_commit(c.last) }
+    @bitly = bitly(@repo)
+    payload["commits"].each { |c| process_commit(c) }
   end
-  
+
   def connect(repo)
     credentials = REPOS[repo]
-    return Twitter::Base.new(credentials['username'], credentials['password'])
+    httpauth = Twitter::HTTPAuth.new(credentials['username'], credentials['password'])
+    return Twitter::Base.new(httpauth)
   end
-  
+
   def process_commit(commit)
     commit["repo"] = @repo
-    proc = Proc.new do 
+    proc = Proc.new do
       commit
     end
-    @twitter.post(@template.result(proc))
+    commit['url'] = @bitly.shorten(commit['url']).short_url if @bitly
+    @twitter.update(@template.result(proc))
   end
-  
+
+  def bitly(repo)
+    credentials = REPOS[repo]
+    return nil unless credentials['bitly_username'] && credentials['bitly_api_key']
+    Bitly.new(credentials['bitly_username'], credentials['bitly_api_key'])
+  end
 end
 
 post '/' do
